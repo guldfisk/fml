59530782301a354a09af1224b38cff006e746875
guldfisk <ce.guldfisk@gmail.com>
2021-08-06 15:33:07 +0200

    ok

--- fml/client/client.py
+++ fml/client/client.py
@@ -17,7 +17,7 @@ from fml.client import models, values
 from fml.client import output
 from fml.client.dtmath.parse import DTMParseException
 from fml.client.output import show_points
-from fml.client.utils import format_timedelta
+from fml.client.utils import format_timedelta, format_timedelta_years
 from fml.client.values import ALARM_DATETIME_FORMAT
 
 
@@ -1220,7 +1220,20 @@ def change_priority(
 
 @main.command(name = 'dt')
 @click.argument('args', type = str, required = True, nargs = -1)
-def dt_math(args: t.Sequence[str]):
+@click.option(
+    '--format-years',
+    '-y',
+    default = False,
+    type = bool,
+    is_flag = True,
+    show_default = True,
+    help = 'Approximate years as 365 days when formatting time delta.',
+)
+def dt_math(args: t.Sequence[str], format_years: bool):
+    """
+    Datetime calculator.
+    Time to next year: ".1/jan - n".
+    """
     from fml.client.dtmath.parse import DTMParser
 
     try:
@@ -1231,7 +1244,13 @@ def dt_math(args: t.Sequence[str]):
         print('can\'t add dates')
     else:
         if isinstance(result, datetime.timedelta):
-            print('delta: ' + format_timedelta(result))
+            print(
+                'delta: ' + (
+                    format_timedelta_years
+                    if format_years else
+                    format_timedelta
+                )(result)
+            )
         else:
             print('time: ' + result.strftime(ALARM_DATETIME_FORMAT))
 
--- fml/client/utils.py
+++ fml/client/utils.py
@@ -1,5 +1,32 @@
 import datetime
 
 
+def plural(n):
+    return abs(n) != 1 and 's' or ''
+
+
 def format_timedelta(delta: datetime.timedelta) -> str:
-    return str(delta).split('.')[0]
+    mm, ss = divmod(delta.seconds, 60)
+    hh, mm = divmod(mm, 60)
+    s = f'{hh:d}:{mm:02d}:{ss:02d}'
+
+    if delta.days:
+        s = f'{delta.days:d} day{plural(delta.days)}, ' + s
+
+    return s
+
+
+def format_timedelta_years(delta: datetime.timedelta) -> str:
+    mm, ss = divmod(delta.seconds, 60)
+    hh, mm = divmod(mm, 60)
+    s = f'{hh:d}:{mm:02d}:{ss:02d}'
+
+    if delta.days:
+        y, d = divmod(delta.days, 365)
+
+        s = f'{d:d} day{plural(d)}, ' + s
+
+        if y:
+            s = f'{y:d} year{plural(y)}, ' + s
+
+    return s
